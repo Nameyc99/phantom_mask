1e26579b164f65554528185158456daa4787c388
nyc <test@example.com>
2025-05-27 16:23:34 +0800

    Fix broken url "api/schema/"

--- phantom_mask/urls.py
+++ phantom_mask/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     path('api/', include('core.urls')),
 
     # OpenAPI schema
-    # path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
 
     # Swagger UI
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
